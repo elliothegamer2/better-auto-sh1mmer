1687d56ea2df064c25c262bb65dc82ff30fe59d7
elliothegamer2 <127466749+elliothegamer2@users.noreply.github.com>
2023-08-02 20:55:51 -0500

    Update shim.sh

--- shim.sh
+++ shim.sh
@@ -1,5 +1,10 @@
+echo "Installing required things"
+
+
+sudo apt install cgpt
 sudo apt-get unzip
 
+clear
 echo "Download RMA shi"
 echo "The board you are download is:" $1
 echo "Are you sure"
@@ -14,6 +19,32 @@ file="https://dl.osu.bio/api/raw/?path=/"$1".zip"
 
 wget $file
 
-unzip index.htmlpath=%2F$1
+path="~/auto-sh1mmer-better/"$1
+echo $path
+sleep 1
+clear
 
-echo "Finished"
+unzip index.html?path=%2F$1
+
+read -p "do you want a dev shim? [Y/N]: " inject
+
+if [[ $inject == "Y" || $inject == "y" ]]; then
+    path=$path" --dev"
+    echo "Injecting with dev"
+    sleep 1
+    git clone https://github.com/CoolElectronics/sh1mmer
+    cd sh1mmer/wax
+    wget https://dl.sh1mmer.me/build-tools/chromebrew/chromebrew-dev.tar.gz
+    sudo bash wax.sh $path
+
+        elif [[ $injet == "N" || $inject == "n" ]]; then
+            echo "Injecting without dev"
+            sleep 1
+            git clone https://github.com/CoolElectronics/sh1mmer
+            cd sh1mmer/wax
+            wget https://dl.sh1mmer.me/build-tools/chromebrew/chromebrew.tar.gz
+            sudo bash wax.sh $path
+        fi
+fi
+
+echo "You shim has finished building (i hope) it should be at ~/auto-sh1mmer-better/"$1
