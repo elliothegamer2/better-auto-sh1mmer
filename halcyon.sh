725189e71fec29f03247cef46ab53e227e622c01
elliothegamer2 <127466749+elliothegamer2@users.noreply.github.com>
2023-08-03 16:59:17 -0500

    lol im dumb

--- halcyon.sh
+++ halcyon.sh
@@ -27,98 +27,98 @@ clear
 
 if [[ $1 == "octopus" ]]; then
     wget https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_15117.111.0_octopus_recovery_stable-channel_mp-v31.bin.zip
-    unzip ~/auto-sh1mmer-better/chromeos_15117.111.0_octopus_recovery_stable-channel_mp-v31.bin.zip
+    unzip chromeos_15117.111.0_octopus_recovery_stable-channel_mp-v31.bin.zip
     mv chromeos_15117.111.0_octopus_recovery_stable-channel_mp-v31.bin octopus107.bin
     buil="1"
 fi
 
 if [[ $1 == "dedede" ]]; then
     wget https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_15117.112.0_dedede_recovery_stable-channel_mp-v27.bin.zip
-    unzip ~/auto-sh1mmer-better/chromeos_15117.112.0_dedede_recovery_stable-channel_mp-v31.bin.zip
+    unzip chromeos_15117.112.0_dedede_recovery_stable-channel_mp-v31.bin.zip
     mv chromeos_15117.112.0_dedede_recovery_stable-channel_mp-v31.bin dedede107.bin
     buil="1"
 fi
 
 if [[ $1 == "strongbad" ]]; then
     wget https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_15117.112.0_strongbad_recovery_stable-channel_mp-v5.bin.zip
-    unzip ~/auto-sh1mmer-better/chromeos_15117.112.0_strongbad_recovery_stable-channel_mp-v5.bin.zip
+    unzip chromeos_15117.112.0_strongbad_recovery_stable-channel_mp-v5.bin.zip
     mv chromeos_15117.112.0_strongbad_recovery_stable-channel_mp-v5.bin strongbad107.bin
     buil="1"
 fi
 
 if [[ $1 == "byra" ]]; then
     wget https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_15117.111.0_brya_recovery_stable-channel_mp-v9.bin.zip
-    unzip ~/auto-sh1mmer-better/chromeos_15117.111.0_brya_recovery_stable-channel_mp-v9.bin.zip
+    unzip chromeos_15117.111.0_brya_recovery_stable-channel_mp-v9.bin.zip
     mv chromeos_15117.111.0_brya_recovery_stable-channel_mp-v9.bin byra107.bin
     buil="1"
 fi
 
 if [[ $1 == "coral" ]]; then
     wget https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_15117.111.0_coral_recovery_stable-channel_mp-v19.bin.zip
-    unzip ~/auto-sh1mmer-better/chromeos_15117.111.0_coral_recovery_stable-channel_mp-v19.bin.zip
+    unzip chromeos_15117.111.0_coral_recovery_stable-channel_mp-v19.bin.zip
     mv chromeos_15117.111.0_coral_recovery_stable-channel_mp-v19.bin coral107.bin
     buil="1"
 fi
 
 if [[ $1 == "grunt" ]]; then
     wget https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_15117.111.0_grunt_recovery_stable-channel_mp-v6.bin.zip
-    unzip ~/auto-sh1mmer-better/chromeos_15117.111.0_grunt_recovery_stable-channel_mp-v6.bin.zip
+    unzip chromeos_15117.111.0_grunt_recovery_stable-channel_mp-v6.bin.zip
     mv chromeos_15117.111.0_grunt_recovery_stable-channel_mp-v6.bin grunt107.bin
     buil="1"
 fi
 
 if [[ $1 == "hana" ]]; then
     wget https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_15117.111.0_hana_recovery_stable-channel_mp-v7.bin.zip
-    unzip ~/auto-sh1mmer-better/chromeos_15117.111.0_hana_recovery_stable-channel_mp-v7.bin.zip
+    unzip chromeos_15117.111.0_hana_recovery_stable-channel_mp-v7.bin.zip
     mv chromeos_15117.111.0_hana_recovery_stable-channel_mp-v7.bin hana107.bin
     buil="1"
 fi
 
 if [[ $1 == "hatch" ]]; then
     wget https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_15117.112.0_hatch_recovery_stable-channel_mp-v6.bin.zip
-    unzip ~/auto-sh1mmer-better/chromeos_15117.112.0_hatch_recovery_stable-channel_mp-v6.bin.zip
+    unzip chromeos_15117.112.0_hatch_recovery_stable-channel_mp-v6.bin.zip
     mv chromeos_15117.112.0_hatch_recovery_stable-channel_mp-v6.bin hatch107.bin
     buil="1"
 fi
 
 if [[ $1 == "jacuzzi" ]]; then
     wget https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_15117.111.0_jacuzzi_recovery_stable-channel_mp-v7.bin.zip
-    unzip ~/auto-sh1mmer-better/chromeos_15117.111.0_jacuzzi_recovery_stable-channel_mp-v7.bin.zip
+    unzip chromeos_15117.111.0_jacuzzi_recovery_stable-channel_mp-v7.bin.zip
     mv chromeos_15117.111.0_jacuzzi_recovery_stable-channel_mp-v7.bin jacuzzi107.bin
     buil="1"
 fi
 
 if [[ $1 == "sentry" ]]; then
     wget https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_15117.112.0_sentry_recovery_stable-channel_mp.bin.zip
-    unzip ~/auto-sh1mmer-better/chromeos_15117.112.0_sentry_recovery_stable-channel_mp.bin.zip
+    unzip chromeos_15117.112.0_sentry_recovery_stable-channel_mp.bin.zip
     mv chromeos_15117.112.0_sentry_recovery_stable-channel_mp.bin sentry107.bin
     buil="1"
 fi
 
 if [[ $1 == "kukui" ]]; then
     wget https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_15117.112.0_kukui_recovery_stable-channel_mp-v4.bin.zip
-    unzip ~/auto-sh1mmer-better/chromeos_15117.112.0_kukui_recovery_stable-channel_mp-v4.bin.zip
+    unzip chromeos_15117.112.0_kukui_recovery_stable-channel_mp-v4.bin.zip
     mv chromeos_15117.112.0_kukui_recovery_stable-channel_mp-v4.bin kukui107.bin
     buil="1"
 fi
 
 if [[ $1 == "nami" ]]; then
     wget https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_15117.112.0_nami_recovery_stable-channel_mp-v5.bin.zip
-    unzip ~/auto-sh1mmer-better/chromeos_15117.112.0_nami_recovery_stable-channel_mp-v5.bin.zip
+    unzip chromeos_15117.112.0_nami_recovery_stable-channel_mp-v5.bin.zip
     mv chromeos_15117.112.0_nami_recovery_stable-channel_mp-v5.bin nami107.bin
     buil="1"
 fi
 
 if [[ $1 == "volteer" ]]; then
     wget https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_15117.112.0_volteer_recovery_stable-channel_mp-v8.bin.zip
-    unzip ~/auto-sh1mmer-better/chromeos_15117.112.0_volteer_recovery_stable-channel_mp-v8.bin.zip
+    unzip chromeos_15117.112.0_volteer_recovery_stable-channel_mp-v8.bin.zip
     mv chromeos_15117.112.0_volteer_recovery_stable-channel_mp-v8.bin volteer107.bin
     buil="1"
 fi
 
 if [[ $1 == "zork" ]]; then
     wget https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_15117.112.0_zork_recovery_stable-channel_mp-v7.bin.zip
-    unzip ~/auto-sh1mmer-better/chromeos_15117.112.0_zork_recovery_stable-channel_mp-v7.bin.zip
+    unzip chromeos_15117.112.0_zork_recovery_stable-channel_mp-v7.bin.zip
     mv chromeos_15117.112.0_zork_recovery_stable-channel_mp-v7.bin zork107.bin
     buil="1"
 fi
@@ -157,7 +157,7 @@ if [[ $buil = "1" ]]; then
        git clone https://github.com/MercuryWorkshop/RecoMod
         cd RecoMod
         chmod +x recomod.sh
-        sudo ./recomod.sh -i ~/auto-sh1mmer-better/coral107.bin --halcyon --rw_legacy
+        sudo ./recomod.sh -i coral107.bin --halcyon --rw_legacy
     fi
 
     if [[ $1 == "grunt" ]]; then
