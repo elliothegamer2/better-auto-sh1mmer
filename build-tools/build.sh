7ab2352f9134e55f697942d9e566d72a9f66a68a
elliothegamer2 <127466749+elliothegamer2@users.noreply.github.com>
2023-08-05 07:02:16 -0500

    Create build.sh

---
+++ build-tools/build.sh
@@ -0,0 +1,23 @@
+file="~/better-auto-sh1mmer/"$1".bin"
+echo "Building: " $file
+
+
+ask() {
+    read -p "Dev?[y/n]" dev
+
+    if [[ $dev == "y" || $dev == "Y" ]]; then
+        git clone https://github.com/CoolElectronics/sh1mmer
+        cd sh1mmer/wax
+        wget https://dl.sh1mmer.me/build-tools/chromebrew/chromebrew-dev.tar.gz
+        sudo bash wax.sh $file --dev
+    elif [[ $dev == "n" || $dev == "N" ]]; then
+        git clone https://github.com/CoolElectronics/sh1mmer
+        cd sh1mmer/wax
+        wget https://dl.sh1mmer.me/build-tools/chromebrew/chromebrew.tar.gz
+        sudo bash wax.sh $file
+
+        elif [[ $file != "y" || $file != "Y" || $file != "n" || $file != "N" ]]; then
+        ask
+    fi
+}
+ask
